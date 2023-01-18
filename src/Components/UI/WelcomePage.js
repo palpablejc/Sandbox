6dd1f05074f28a47c066d5bef00d459bcd89a8e7
palpablejc <77988503+palpablejc@users.noreply.github.com>
2023-01-17 20:14:41 -0600

    Testing Netlify deployment

---
+++ src/Components/UI/WelcomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const WelcomePage = () => {
+
+    return <div>
+        <h1>Build / Deployment POC</h1>
+        <p>The GitHub Repo housing this app is using Netlify to deploy when the main branch is updated.</p>
+    </div>
+}
+
+export default WelcomePage;
