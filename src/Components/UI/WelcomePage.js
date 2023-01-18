f2bb45712a4e236999e2412dadf5a7276b0e0b29
palpablejc <77988503+palpablejc@users.noreply.github.com>
2023-01-18 15:51:54 -0600

    Update WelcomePage.js

--- src/Components/UI/WelcomePage.js
+++ src/Components/UI/WelcomePage.js
@@ -6,6 +6,7 @@ const WelcomePage = () => {
     return <div>
         <h1>Build / Deployment POC</h1>
         <p>The GitHub Repo housing this app is using Netlify to deploy when the main branch is updated.</p>
+        <p>Sup Roni</p>
     </div>
 }
 
